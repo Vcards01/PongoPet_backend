dd31426a52857fd9c9f53d43f27c3dd87f625b78
Victor Cardoso <v.h.cardoso30011999@hotmail.com>
2020-07-21 20:09:22 -0300

    Inicio autenticação

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,9 +1,9 @@
 //index,show,update,destroy
-const Cliente = require("../models/UserCliente")
+const Cliente = require("../models/UserCliente");
+const bcrypt = require('bcryptjs');
 
 module.exports={
     async store(req,res){
-        var exists=true;
         const {name} = req.body;
         const {email} = req.body;
         const {password} = req.body;
@@ -11,20 +11,25 @@ module.exports={
         let user = await Cliente.findOne({email});
         if (!user){
             user = await Cliente.create({name,email,password})
-            exists=false
         }
-        return res.json({user,exists})
+        else{
+            return res.status(400).send({error:'usuario ja cadastrado'})
+        }
+        return res.json({user})
     },
 
     async show(req,res){
         var token="";
         var exists=true;
-        const{email}=req.body;
-        const{password}=req.body;
+        const{email,password}=req.body;
 
         let user = await Cliente.findOne({email});
         if (!user){
             exists=false;
+        }
+        if(!await bcrypt.compare(password,user.password))
+        {
+
         }
         else{
             if (user.password === password)
--- src/models/UserCliente.js
+++ src/models/UserCliente.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: String,
     email:String,
-    password:String,
+    password:{
+        type:String,
+        required:true,
+        select:false,
+    },
     address:{
         zipCode:String,
         city:String,
@@ -16,6 +21,12 @@ const UserSchema = new mongoose.Schema({
         price:Number,
         quantity:Number,
     }],
+});
+
+UserSchema.pre('save',async function(next){
+    const hash = await bcrypt.hash(this.password,10);
+    this.password=hash;
+    next();
 });
 
 module.exports = mongoose.model('Cliente',UserSchema)
